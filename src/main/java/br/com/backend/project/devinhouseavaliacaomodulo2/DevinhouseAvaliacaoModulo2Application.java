d4f7d2c742957765159764205d5cd986d132a3c0
David Mendonça <davidmed66@gmail.com>
2021-04-19 20:32:31 -0300

    primeiro commit

---
+++ src/main/java/br/com/backend/project/devinhouseavaliacaomodulo2/DevinhouseAvaliacaoModulo2Application.java
@@ -0,0 +1,13 @@
+package br.com.backend.project.devinhouseavaliacaomodulo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinhouseAvaliacaoModulo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinhouseAvaliacaoModulo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/backend/project/devinhouseavaliacaomodulo2/DevinhouseAvaliacaoModulo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.project.devinhouseavaliacaomodulo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevinhouseAvaliacaoModulo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
