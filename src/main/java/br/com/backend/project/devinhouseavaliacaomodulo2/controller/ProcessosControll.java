553eeff5be1cc3b269d688d34043f5b0a029ed83
David Mendonça <davidmed66@gmail.com>
2021-04-20 19:32:58 -0300

    adicionado versionamento

--- src/main/java/br/com/backend/project/devinhouseavaliacaomodulo2/controller/ProcessosControll.java
+++ src/main/java/br/com/backend/project/devinhouseavaliacaomodulo2/controller/ProcessosControll.java
@@ -23,33 +23,33 @@ public class ProcessosControll {
 	private ProcessoService service;
 	//TODO: Fazer o versionamento de tudo.
 	
-	@RequestMapping(value = "/v1" + "/criar-processo", method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(headers = "api-version=v1", value = "/v1" + "/criar-processo", method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 	public List<ProcessoDTO> criarProcesso(@RequestBody ProcessoDTO novoProcesso) {
 		return service.criarProcesso(novoProcesso);
 	}
 	
-	@RequestMapping(value = "/v1" + "/consultar-todos-processos", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE )
+	@RequestMapping(headers = "api-version=v1", value = "/v1" + "/consultar-todos-processos", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE )
 	public List<ProcessoDTO> consultarTodosOsProcessos() {
 		return service.retornarTodosOsProcessos();
 	}
 	
-	@RequestMapping(value = "/v1" + "/consultar-processo-porid", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(headers = "api-version=v1", value = "/v1" + "/consultar-processo-porid", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
 	//quando pega o parametro da url, retorna um String sempre?
 	public ProcessoDTO consultarProcessoPorId(@RequestParam Integer id) {
 		return service.retornarProcessoPorId(id);
 	}
 	
-	@RequestMapping(value = "/v1" +"/consultar-processo-porchave", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
-	public ProcessoDTO consultarProcessoPorChaveDeProcesso(@RequestParam String chaveDeProcesso) {
-		return service.retornarProcessoPorChaveDeProcesso(chaveDeProcesso);
+	@RequestMapping(headers = "api-version=v1", value = "/v1" +"/consultar-processo-porchave", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+	public ProcessoDTO consultarProcessoPorChaveDeProcesso(@RequestParam String chave) {
+		return service.retornarProcessoPorChaveDeProcesso(chave);
 	}
 
-	@RequestMapping(value = "/v1" + "/atualizar-processo", method = RequestMethod.PUT, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE )
+	@RequestMapping(headers = "api-version=v1", value = "/v1" + "/atualizar-processo", method = RequestMethod.PUT, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE )
 	public List<ProcessoDTO> atualizarProcesso(@RequestParam Integer id, @RequestBody ProcessoDTO processoAtualizado) {
 		return service.atualizarProcesso(id, processoAtualizado);
 	}
 	
-	@RequestMapping(value = "/v1" +"/deletar-processo", method = RequestMethod.DELETE, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(headers = "api-version=v1", value = "/v1" +"/deletar-processo", method = RequestMethod.DELETE, produces = APPLICATION_JSON_VALUE)
 	public List<ProcessoDTO> deletarProcesso(@RequestParam Integer id) {
 		return service.deletarProcesso(id);
 	}
